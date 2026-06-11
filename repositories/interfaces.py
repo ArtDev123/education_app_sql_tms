b4ebf08a27b956033617051b2150904b8884c36d
Artem <temaartem240904@gmail.com>
2026-06-11 21:11:21 +0300

    added interfaces

---
+++ repositories/interfaces.py
@@ -0,0 +1,48 @@
+"""Интерфейсы (абстрактные контракты) репозиториев."""
+
+from abc import ABC, abstractmethod
+from typing import Generic, Optional, TypeVar
+
+# from models.entities import Course, Direction, Student, StudentResult, Teacher
+
+T = TypeVar("T")
+
+
+class IRepository(ABC, Generic[T]):
+    """Базовый интерфейс репозитория с CRUD-операциями."""
+
+    @abstractmethod
+    def add(self, entity: T) -> int:
+        """Добавить сущность и вернуть её ID."""
+
+    @abstractmethod
+    def get_by_id(self, entity_id: int) -> Optional[T]:
+        """Получить сущность по ID."""
+
+    @abstractmethod
+    def get_all(self) -> list[T]:
+        """Получить все сущности."""
+
+    @abstractmethod
+    def update(self, entity: T) -> bool:
+        """Обновить сущность."""
+
+    @abstractmethod
+    def delete(self, entity_id: int) -> bool:
+        """Удалить сущность по ID."""
+
+
+class ISearchableRepository(IRepository[T], ABC):
+    """Интерфейс репозитория с поиском по частичному совпадению имени."""
+
+    @abstractmethod
+    def search_by_name(self, pattern: str) -> list[T]:
+        """Найти сущности по фрагменту имени или названия."""
+
+
+# class IDirectionRepository(ISearchableRepository[Direction], ABC):
+#     """Интерфейс репозитория учебных направлений."""
+
+#     @abstractmethod
+#     def get_by_name(self, name: str) -> Optional[Direction]:
+#         """Получить направление по точному названию."""
